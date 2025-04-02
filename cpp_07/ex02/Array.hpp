24efa0d1c0927849c02ec482306f8ee6134c0b1c
Antoine Monot <amonot@paul-f3Br4s4.clusters.42paris.fr>
2025-04-02 17:32:46 +0200

    asdf

--- cpp_07/ex02/Array.hpp
+++ cpp_07/ex02/Array.hpp
@@ -6,14 +6,89 @@
 /*   By: amonot <amonot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 20:30:27 by amonot            #+#    #+#             */
-/*   Updated: 2025/03/28 17:13:37 by amonot           ###   ########.fr       */
+/*   Updated: 2025/04/02 17:22:26 by amonot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <cstdlib>
+#include <exception>
+#include <iostream>
 
 template< typename T >
 class Array {
 	
+	private:
+		T				*_tab;
+		unsigned int	_size;
+
 	public:
-		Array<T>(void);
+		Array(void);
+		Array(unsigned int size);
+		Array(const Array<T> &other);
+		~Array(void);
+
+		T& operator[](unsigned int i);
+		Array<T>& operator=(const Array<T> &other);
 		
+		class IndexOutOfBoundsException : public std::exception {
+			public:
+				const char* what() const throw() {
+					return ("Index is out of bounds");
+				}
+		};
 };
+
+template< typename T >
+Array<T>::Array(void)
+{
+	_tab = new T[0];
+	_size = 0;
+}
+
+template< typename T >
+Array<T>::Array(unsigned int size)
+{
+	_tab = new T[size];
+	_size = size;
+}
+
+template< typename T >
+Array<T>::Array(const Array<T> &other)
+{
+	if (this != &other)
+	{
+		_tab = new T[other._size];
+		_size = other._size;
+		for (unsigned int i = 0; i < other._size; i++)
+			_tab[i] = other._tab[i];
+	}
+}
+
+template< typename T >
+T& Array<T>::operator[](unsigned int i)
+{
+	if (i >= _size)
+		throw IndexOutOfBoundsException();
+	return (_tab[i]);
+}
+
+template< typename T >
+Array<T>& Array<T>::operator=(const Array<T> &other)
+{
+	std::cout << "operator = of Array" << std::endl;
+	if (this != &other)
+	{
+		delete[] _tab;
+		_tab = new T[other._size];
+		_size = other._size;
+		for (unsigned int i = 0; i < other._size; i++)
+			_tab[i] = other._tab[i];
+	}
+	return (*this);
+}
+
+template< typename T >
+Array<T>::~Array(void)
+{
+	delete[] _tab;
+}
--- cpp_07/ex02/main.cpp
+++ cpp_07/ex02/main.cpp
@@ -6,11 +6,71 @@
 /*   By: amonot <amonot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 20:29:49 by amonot            #+#    #+#             */
-/*   Updated: 2025/03/25 20:31:08 by amonot           ###   ########.fr       */
+/*   Updated: 2025/04/02 17:31:50 by amonot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Array.hpp"
+
 int main(void)
 {
+	Array<int> *a = new Array<int>(3);
+	Array<int> *b = new Array<int>(3);
+
+	(*a)[0] = 1;
+	(*a)[1] = 2;
+	(*a)[2] = 3;
 	
+	std::cout << (*a)[1] << std::endl;
+	std::cout << (*b)[1] << std::endl;
+	*b = *a;
+	std::cout << std::endl;
+	std::cout << (*a)[1] << std::endl;
+	std::cout << (*b)[1] << std::endl;
+	(*b)[1] = 42;
+	std::cout << std::endl;
+	std::cout << (*a)[1] << std::endl;
+	std::cout << (*b)[1] << std::endl;
+	delete a;
+	std::cout << std::endl;
+	std::cout << (*b)[1] << std::endl;
+
+	delete b;
+	return (0);
 }
+
+
+/* int main(void)
+{
+	Array<int> *a = new Array<int>(3);
+	Array<int> *b = new Array<int>(3);
+
+	*b = *a;
+
+	delete a;
+	delete b;
+
+	return (0);
+} */
+
+
+/* class test {
+	public :
+		test(void) {}
+		~test(void) {}
+
+		void operator=(const test &other) {
+			(void)other;
+			std::cout << "operator = de test" << std::endl;
+		}
+};
+
+int main(void)
+{
+	test a;
+	test b;
+
+	a = b;
+	return (0);
+} */
