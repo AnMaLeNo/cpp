28121844d599c1067b9e639ec7d153816bb45dc3
AnMaLeNo <antoineblr16@gmail.com>
2025-04-04 00:10:25 +0200

    cpp08 ex00

--- cpp_08/ex00/easyfind.hpp
+++ cpp_08/ex00/easyfind.hpp
@@ -3,17 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   easyfind.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amonot <amonot@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amonot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 17:49:38 by amonot            #+#    #+#             */
-/*   Updated: 2025/04/03 18:54:30 by amonot           ###   ########.fr       */
+/*   Updated: 2025/04/03 23:54:15 by amonot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <algorithm>
+#include <exception>
 
 template < typename T >
-int &easyfind(T type, int nbr)
+int *easyfind(T& type, int nbr)
 {
-	return (std::find(type.begin(), type.end(), nbr));
+	typename T::iterator it;
+	it = std::find(type.begin(), type.end(), nbr);
+	if (it == type.end())
+		return (NULL);
+	return (&(*it));
 }
--- cpp_08/ex00/main.cpp
+++ cpp_08/ex00/main.cpp
@@ -3,20 +3,48 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amonot <amonot@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amonot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 17:48:29 by amonot            #+#    #+#             */
-/*   Updated: 2025/04/03 18:55:25 by amonot           ###   ########.fr       */
+/*   Updated: 2025/04/04 00:08:13 by amonot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <array>
+#include <vector>
+#include <deque>
 #include "easyfind.hpp"
+
+/* int main(void)
+{
+	std::vector<int> array(3);
+	int *i;
+
+	array[0] = 1;
+	array[1] = 2;
+	array[2] = 3;
+	i = easyfind(array, 1);
+	if (i != NULL)
+	{
+		std::cout << *i << std::endl;
+		*i = 9;
+	}
+	std::cout << array[0] << array[1] << array[2] << std::endl;
+} */
 
 int main(void)
 {
-	std::array<int, 5> array = { 2, 16, 77, 34, 50 };
+	std::deque<int> array(4, 100);
+	int *i;
 
-	std::cout << easyfind(array, 16) << std::endl;
+	array[0] = 1;
+	array[1] = 2;
+	array[2] = 3;
+	i = easyfind(array, 100);
+	if (i != NULL)
+	{
+		std::cout << *i << std::endl;
+		*i = 9;
+	}
+	std::cout << array[0] << array[1] << array[2] << array[3] << std::endl;
 }
