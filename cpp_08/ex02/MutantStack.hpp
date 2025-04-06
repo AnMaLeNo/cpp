b8a59f8eda4794971488eb44400c09b9eb2896c8
AnMaLeNo <antoineblr16@gmail.com>
2025-04-06 02:26:16 +0200

    add iterator for all type in ex02

--- cpp_08/ex02/MutantStack.hpp
+++ cpp_08/ex02/MutantStack.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   MutantStack.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amonot <amonot@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amonot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 16:16:58 by amonot            #+#    #+#             */
-/*   Updated: 2025/04/04 19:43:29 by amonot           ###   ########.fr       */
+/*   Updated: 2025/04/06 02:23:40 by amonot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,24 @@ class MutantStack : public std::stack<T> {
 	public:
 		MutantStack(void);
 
-		typename std::stack<int>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::iterator iterator;
 		
-		std::stack<int>::container_type::iterator begin(void) {
-			return (this->c.begin());
-		}
-		std::stack<int>::container_type::iterator end(void) {
-			return (this->c.end());
-		}
+		iterator begin(void);
+		iterator end(void);
 };
 
 template < typename T >
 MutantStack<T>::MutantStack(void)
 {}
 
-/* template < typename T >
-iterator MutantStack<T>::begin(void)
+template < typename T >
+typename MutantStack<T>::iterator MutantStack<T>::begin(void)
 {
 	return (this->c.begin());
 }
 
 template < typename T >
-iterator MutantStack<T>::end(void)
+typename MutantStack<T>::iterator MutantStack<T>::end(void)
 {
 	return (this->c.end());
-} */
+}
--- cpp_08/ex02/main.cpp
+++ cpp_08/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amonot <amonot@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amonot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 16:16:10 by amonot            #+#    #+#             */
-/*   Updated: 2025/04/04 19:46:33 by amonot           ###   ########.fr       */
+/*   Updated: 2025/04/06 02:24:26 by amonot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 #include <vector>
 #include <stack>
 #include "MutantStack.hpp"
+#include "list"
 
-int main(void)
+/* int main(void)
 {
 	MutantStack<int> v;
 
@@ -28,4 +29,66 @@ int main(void)
 	std::cout << v.top()  << std::endl;
 	std::cout << *(v.begin() + 1)  << std::endl;
 	return (0);
+} */
+
+int main()
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
 }
+
+/* int main()
+{
+	std::list<int> mstack;
+	mstack.push_back(5);
+	mstack.push_back(17);
+	std::cout << mstack.back() << std::endl;
+	mstack.pop_back();
+	std::cout << mstack.size() << std::endl;
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	//[...]
+	mstack.push_back(0);
+
+	std::list<int>::iterator it = mstack.begin();
+	std::list<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	std::cout << *it << std::endl;
+	++it;
+	}
+	std::list<int> s(mstack);
+	return 0;
+} */
+
+// 17
+// 1
+// 5
+// 3
+// 5
+// 737
+// 0
